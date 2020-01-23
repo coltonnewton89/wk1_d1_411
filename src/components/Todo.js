a3f15f2af079893fe1c8db7275905dd35d910ec5
coltonnewton89 <coltonnewton89@gmail.com>
2020-01-23 00:24:40 -0600

    finished

---
+++ src/components/Todo.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+    state = {
+        text: '',
+        listItems: []
+    }
+
+    handleTextChange = (e) =>{
+        this.setState({text: e.taget.value})
+    }
+
+    handleClicked = () =>{
+        const items = [ ...this.state.listItems ]
+        items.push(this.state.text)
+        this.setState({listItems: items})
+    }
+    
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={this.handleTextChange} />
+                <button onClick={this.handleClicked}>Add Task</button>
+                {
+                    this.state.listItems.map(value => {
+                    return <p>{value}</p>
+                    })
+                }
+                <p>testing</p>
+            </div>
+        )
+    }
+}
+    
+
+export default Todo
